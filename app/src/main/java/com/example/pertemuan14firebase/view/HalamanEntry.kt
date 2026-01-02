0e31b5e7621409c799d0305d0590b45d8dd3c8e5
adilaroisa <adilaroisa688@gmail.com>
2026-01-02 17:52:59 +0700

    menambahkan import di HalamanEntry

--- app/src/main/java/com/example/pertemuan14firebase/view/HalamanEntry.kt
+++ app/src/main/java/com/example/pertemuan14firebase/view/HalamanEntry.kt
@@ -1,5 +1,36 @@
 package com.example.pertemuan14firebase.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.pertemuan14firebase.view.route.DestinasiEntry
+import com.example.pertemuan14firebase.viewmodel.EntryViewModel
+import com.example.pertemuan14firebase.R
+import com.example.pertemuan14firebase.modeldata.DetailSiswa
+import com.example.pertemuan14firebase.modeldata.UIStateSiswa
+import com.example.pertemuan14firebase.viewmodel.PenyediaViewModel
+import kotlinx.coroutines.launch
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun EntrySiswaScreen(
