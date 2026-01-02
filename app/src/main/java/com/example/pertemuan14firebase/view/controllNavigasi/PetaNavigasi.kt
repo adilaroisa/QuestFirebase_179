82f12f3a8398823109e8d70dbe1a176cfd170a13
adilaroisa <adilaroisa688@gmail.com>
2026-01-02 17:23:04 +0700

    membuat fungsi DataSiswaApp untuk inisialisasi NavController

---
+++ app/src/main/java/com/example/pertemuan14firebase/view/controllNavigasi/PetaNavigasi.kt
@@ -0,0 +1,9 @@
+package com.example.pertemuan14firebase.view.controllNavigasi
+
+
+@Composable
+fun DataSiswaApp(navController: NavHostController = rememberNavController(), modifier:
+Modifier){
+    HostNavigasi(navController = navController)
+}
+
