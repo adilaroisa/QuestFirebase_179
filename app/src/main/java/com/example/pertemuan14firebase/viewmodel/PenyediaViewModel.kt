f3606a2f4b0d30141ad558a2b10ef6adb13ca99d
adilaroisa <adilaroisa688@gmail.com>
2026-01-02 17:52:20 +0700

    menambahkan import di PenyediaViewModel

--- app/src/main/java/com/example/pertemuan14firebase/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/pertemuan14firebase/viewmodel/PenyediaViewModel.kt
@@ -1,5 +1,11 @@
 package com.example.pertemuan14firebase.viewmodel
 
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.pertemuan14firebase.repositori.AplikasiDataSiswa
+
 fun CreationExtras.aplikasiDataSiswa():AplikasiDataSiswa = (
         this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as
                 AplikasiDataSiswa
