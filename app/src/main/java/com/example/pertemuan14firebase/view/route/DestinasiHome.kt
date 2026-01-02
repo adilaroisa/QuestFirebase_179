7f44dd9e29c02cb5208b01ffd00ab3ecdf7736e9
adilaroisa <adilaroisa688@gmail.com>
2026-01-02 16:44:19 +0700

    menambahkan objek DestinasiHome untuk rute navigasi halaman utama

---
+++ app/src/main/java/com/example/pertemuan14firebase/view/route/DestinasiHome.kt
@@ -0,0 +1,7 @@
+package com.example.pertemuan14firebase.view.route
+import com.example.pertemuan14firebase.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
