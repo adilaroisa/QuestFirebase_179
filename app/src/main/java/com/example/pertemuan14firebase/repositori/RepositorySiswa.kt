b9520bb3d1d518e5b76e297b36ec71204ba423c9
adilaroisa <adilaroisa688@gmail.com>
2026-01-02 17:51:56 +0700

    menambahkan import di RepositorySiswa

--- app/src/main/java/com/example/pertemuan14firebase/repositori/RepositorySiswa.kt
+++ app/src/main/java/com/example/pertemuan14firebase/repositori/RepositorySiswa.kt
@@ -1,6 +1,8 @@
 package com.example.pertemuan14firebase.repositori
 
-
+import com.example.pertemuan14firebase.modeldata.Siswa
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.tasks.await
 
 interface RepositorySiswa {
     suspend fun getDataSiswa(): List<Siswa>
