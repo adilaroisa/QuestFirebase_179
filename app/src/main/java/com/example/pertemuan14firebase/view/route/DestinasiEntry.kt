86fc12cc65535336af9413b5745ffb88df858b9a
adilaroisa <adilaroisa688@gmail.com>
2026-01-02 16:40:45 +0700

    menambahkan objek DestinasiEntry untuk rute navigasi tambah siswa

---
+++ app/src/main/java/com/example/pertemuan14firebase/view/route/DestinasiEntry.kt
@@ -0,0 +1,7 @@
+package com.example.pertemuan14firebase.view.route
+import com.example.pertemuan14firebase.R
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
