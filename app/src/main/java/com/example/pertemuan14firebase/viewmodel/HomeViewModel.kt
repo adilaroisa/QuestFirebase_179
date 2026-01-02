645ed7481edcc2f44b8b87e26a5b26ea08950ab5
adilaroisa <adilaroisa688@gmail.com>
2026-01-02 17:11:38 +0700

    membuat sealed interface StatusUiSiswa untuk handle state halaman home

---
+++ app/src/main/java/com/example/pertemuan14firebase/viewmodel/HomeViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.pertemuan14firebase.viewmodel
+
+sealed interface StatusUiSiswa {
+    data class Success(val siswa: List<Siswa> = listOf()) : StatusUiSiswa
+    object Error : StatusUiSiswa
+    object Loading : StatusUiSiswa
+}
