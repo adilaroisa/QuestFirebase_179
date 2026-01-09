4306987aa6c85832fd9c8646c2a9dab7c43d51d5
adilaroisa <adilaroisa688@gmail.com>
2026-01-09 16:20:27 +0700

    Menambahkan fungsi update UI state dan validasi input form edit

--- app/src/main/java/com/example/pertemuan14firebase/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/pertemuan14firebase/viewmodel/EditViewModel.kt
@@ -15,5 +15,16 @@ class EditViewModel(savedStateHandle: SavedStateHandle, private val repositorySi
         }
     }
 
+    fun updateUiState(detailSiswa: DetailSiswa) {
+        uiStateSiswa =
+            UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput(detailSiswa))
+    }
+
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa ): Boolean {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+
 
 }
