c335313e1db94fde732f8bf16d009a018e335e66
adilaroisa <adilaroisa688@gmail.com>
2026-01-09 17:02:01 +0700

    menambahkan import di PenyediaViewModel.kt

--- app/src/main/java/com/example/pertemuan14firebase/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/pertemuan14firebase/viewmodel/PenyediaViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.pertemuan14firebase.viewmodel
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
