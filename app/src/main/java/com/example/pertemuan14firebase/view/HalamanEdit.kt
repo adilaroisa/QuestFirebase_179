2c506259d92899bef6d57f2f91904bc74b087c82
adilaroisa <adilaroisa688@gmail.com>
2026-01-09 15:40:08 +0700

    mengatur dependensi layar, navigasi dan inisialisasi ViewModel

---
+++ app/src/main/java/com/example/pertemuan14firebase/view/HalamanEdit.kt
@@ -0,0 +1,12 @@
+package com.example.pertemuan14firebase.view
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EditSiswaScreen(
+    navigateBack: () -> Unit,
+    onNavigateUp: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EditViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+
+}
